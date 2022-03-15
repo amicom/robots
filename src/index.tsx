81b6d39de768297121c0813c1d10628226ae579f
Maor <amicom.pro@gmail.com>
2022-03-15 15:27:58 +0200

    implementing MUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
+import { CssBaseline } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './reset.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
